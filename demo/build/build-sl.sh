dba4f9871fe6c679d0957030a5910052af5afbed
Yusuke Endoh <mame@ruby-lang.org>
2023-05-30 21:20:46 +0900

    Fix a file path

--- demo/build/build-sl.sh
+++ demo/build/build-sl.sh
@@ -13,6 +13,6 @@ if [ ! -e $DIR ]; then
   tar xf $FILE
 fi
 
-tar xzf 5.02.tar.gz
+tar xzf $FILE
 cd sl-5.02
 emcc -sNO_EXIT_RUNTIME=0 -sFORCE_FILESYSTEM=1 -o ../../static/sl-core.js sl.c -I$(dirname $PWD)/ncurses-6.3/usr/local/include -I$(dirname $PWD)/ncurses-6.3/usr/local/include/ncurses -L$(dirname $PWD)/ncurses-6.3/usr/local/lib -lncurses -ltinfo
