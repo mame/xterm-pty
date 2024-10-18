bc48d650f6cbe0c43e045680b9e63c08881188e9
Yusuke Endoh <mame@ruby-lang.org>
2024-10-18 11:56:42 +0900

    Do not expose Tty*

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export { openpty } from "./pty";
-export { TtyRequest } from './client-server/termiosData';
-export { TtyClient } from "./client-server/ttyClient";
-export { TtyServer } from "./client-server/ttyServer";
 export { Flags } from './termios';
 export { Termios, TermiosConfig } from './termios';
